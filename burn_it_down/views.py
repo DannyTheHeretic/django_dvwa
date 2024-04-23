3168d56418fefd64a69ed99f6e5d4c3d3bf16269
Daniel <DannyTheHeretic@gmail.com>
2024-04-22 20:41:55 -0500

    views and hosting

--- burn_it_down/views.py
+++ burn_it_down/views.py
@@ -3,25 +3,23 @@ import os
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.db.models.expressions import RawSQL
-from django.db import connection
-import sqlite3
 
 from burn_it_down.models import User
+
 # import burn_it_down.User as User
 # Create your views here.
 
 
-def index(request : HttpRequest) -> HttpResponse:
+def index(request: HttpRequest) -> HttpResponse:
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/index.html',
+        "app/index.html",
         {
-            'title': 'Home Page',
-            'year': datetime.datetime.now().year,
-        }
+            "title": "Home Page",
+            "year": datetime.datetime.now().year,
+        },
     )
 
 
@@ -29,7 +27,8 @@ def index(request : HttpRequest) -> HttpResponse:
 def ping(request):
     url = request.body
     t = os.popen(f"ping {str(url, 'utf-8')}").read()
-    return JsonResponse({'body':t})
+    return JsonResponse({"body": t})
+
 
 @csrf_exempt
 def sql(request):
@@ -40,11 +39,8 @@ def sql(request):
         x = User.objects.raw(q)
         for i in x:
             print(i)
-        return JsonResponse({'body':list(x.query)})
-        
+        return JsonResponse({"body": list(x.query)})
+
     except Exception as e:
         print(f"Error: {e}")
-        return JsonResponse({'error': 'An error occurred'})
-
-    
-    
+        return JsonResponse({"error": "An error occurred"})
